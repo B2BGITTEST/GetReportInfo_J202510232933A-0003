4cd1184e316c2af73702bf074f8cdac98e5a62d6
B2BGITTEST <LvGithub@126.com>
2026-01-05 14:27:59 +0800

    首次上传项目0003

---
+++ Scripts/CommonUtil.js
@@ -0,0 +1,26 @@
+﻿//新窗口
+function fnNewWindow(url, width, height, title) {
+    var obj = document.body;
+    var clientWidth = document.documentElement.clientWidth;
+    var clientHeight = document.documentElement.clientHeight;
+    var objWidth = obj.offsetWidth;
+    var objHeight = obj.offsetHeight;
+    var objTop = obj.scrollTop;
+    var objX = Math.abs((clientWidth - width) / 2);
+    var objY = Math.abs((clientHeight - height) / 2);
+    if(obj.scrollTop<(clientHeight/2))
+        objY = objY + obj.scrollTop/4;
+    if (objY > clientHeight) 
+        objY = Math.abs((clientHeight - height) / 2);
+   
+    //alert(objY);
+    ymPrompt.win({ 
+        message: url, 
+        width: width, 
+        height: height, 
+        title: title, 
+        handler: null, 
+        iframe: true, 
+        winPos: [objX, objY] 
+    });
+}
